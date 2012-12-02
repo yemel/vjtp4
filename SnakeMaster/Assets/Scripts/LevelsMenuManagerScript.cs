37a7551ce700c2d22610ccdd7df91da03007d768
guillermo liss <lissguillermo@gmail.com>
2012-12-02 19:19:17 -0300

    menues y escenas

---
+++ SnakeMaster/Assets/Scripts/LevelsMenuManagerScript.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+
+public class LevelsMenuManagerScript : MonoBehaviour {
+
+	public Font font;
+	
+	void Update(){
+		if(Input.GetButton("Jump")) Application.LoadLevel(1);
+	}
+	
+	void OnGUI(){
+		setTitle();
+		setSubtitle();
+		setMenu();
+	}
+	
+	private void setTitle() {
+		var style = getTitleStyle(Color.white);
+		GUI.Label(new Rect(20, 50, 400, 100), "Snake Master", style);
+	}
+
+	private void setSubtitle() {
+		var style = getTitleStyle(Color.white);
+		style.fontSize = 30;
+		GUI.Label(new Rect(350, 65, 300, 100), "Levels", style);
+	}
+	
+	private void setMenu() {
+		int left = 100;
+		int top = 160;
+		int leftMargin = 200;
+		int topMargin = 230;
+		int menuTop = top;
+		int leftCounter = 0;
+		int topCounter = 0;
+		for(int i = 0; i < 20; i++) {
+			int menuLeft = left+leftMargin*leftCounter++;
+			if(menuLeft > Screen.width - 200) {
+				menuLeft = left;
+				menuTop += topMargin;
+				leftCounter = 1;
+			}
+
+			if(GUI.Button(new Rect(menuLeft, menuTop, 150, 180), "Level " + (i+1))) {
+			//	Application.LoadLevel(1);
+			}
+		}
+	}	
+	
+	private GUIStyle getTitleStyle(Color color){
+		var style = new GUIStyle();
+		style.font = font;
+		style.fontSize = 45;
+		style.normal.textColor = color;
+		style.alignment = TextAnchor.UpperCenter;
+		return style;
+	}
+}
---
+++ SnakeMaster/Assets/Scripts/SplashManagerScript.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using System.Collections;
+
+public class SplashManagerScript : MonoBehaviour {
+	
+	public Font font;
+	
+	void Update(){
+		if(Input.GetButton("Jump")) Application.LoadLevel(1);
+	}
+	
+	void OnGUI(){
+		setTitle();
+		setMenu();
+	}
+
+	private void setTitle() {
+		var style = getTitleStyle(Color.white);
+		GUI.Label(new Rect((Screen.width/2)-300, 70, 600, 100), "Snake Master", style);
+	}	
+	
+	private void setMenu() {
+		var style = getSubtitleStyle(Color.yellow);
+		int left = (Screen.width/2)-170;
+		int top = 300;
+		int menumargin = 50;
+
+		if(GUI.Button(new Rect(left, top, 340, 40), "Start new game", style)) {
+			Application.LoadLevel(1);
+		}
+		
+		if(GUI.Button(new Rect(left, top+menumargin, 340, 40), "Continue last game", style)) {
+			Application.LoadLevel(1);
+		}
+		
+//		if(GUI.Button(new Rect(left, top+menumargin*2, 340, 40), "Levels", style)) {
+//			Application.LoadLevel(1);
+//		}
+
+		if(GUI.Button(new Rect(left, top+menumargin*2, 340, 40), "Quit", style)) {
+			Application.Quit();
+		}		
+	}
+	
+	private GUIStyle getTitleStyle(Color color){
+		var style = new GUIStyle();
+		style.font = font;
+		style.fontSize = 60;
+		style.normal.textColor = color;
+		style.alignment = TextAnchor.UpperCenter;
+		return style;
+	}
+	
+	private GUIStyle getSubtitleStyle(Color color){
+		var style = new GUIStyle(GUI.skin.button);
+		style.font = font;
+		style.fontSize = 25;
+		style.normal.textColor = color;
+		style.hover.textColor = Color.green;
+		style.active.textColor = Color.red;
+
+		
+	    Texture2D tex = new Texture2D(2, 2);
+	    Color[] colors = new Color[4];
+	    tex.SetPixels(colors);
+	    tex.Apply();
+	    style.normal.background = tex;
+		style.hover.background = tex;
+		style.active.background = tex;
+		
+		return style;
+	}
+
+}
